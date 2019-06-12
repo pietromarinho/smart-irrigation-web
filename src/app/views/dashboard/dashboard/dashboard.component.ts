97c030fe9d455502cf740096662882f5dc07495c
Pietro Zanelli Marinho de Souza <pietro.marinho@timeitn.com.br>
2019-06-12 08:57:22 -0400

    settings in dash

--- src/app/views/dashboard/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard/dashboard.component.ts
@@ -1,17 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { ArduinoService } from 'app/service/arduino/arduino.service';
 import { Arduino } from 'app/model/arduino.model';
+import { Plantacao } from 'app/model/plantacao.model';
+import { PlantacaoService } from 'app/service/plantacao/plantacao.service';
+import { Plant } from 'app/model/plant.model';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements AfterViewInit {
+  plantacoes: Plantacao[] = [];
 
   constructor(
+    private plantacaoService: PlantacaoService,
     private arduinoService: ArduinoService
   ) { }
+
+
+  ngAfterViewInit() {
+    this.getEquipes();
+  }
+
+  getEquipes() {
+    this.plantacaoService.getAll().subscribe(
+      success => {
+        this.plantacoes = success;
+      }
+    );
+  }
+
+  getQntPlantas(plantas: Plant[]) {
+    let total = 0;
+    plantas.forEach(planta => {
+      total += 1;
+    });
+    return total;
+  }
 
   ligaDesliga() {
     const arduino: Arduino = new Arduino();
